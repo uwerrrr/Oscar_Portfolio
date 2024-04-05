89bb8f7a7463a9ddf2e8a0ce42f00f0a9dae8512
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-04-05 17:50:03 +1100

    adding nav

--- portfolio-ver2/tailwind.config.ts
+++ portfolio-ver2/tailwind.config.ts
@@ -7,18 +7,29 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      powder: "#fffef8",
-      eggshell: "#efecda",
-      uranian: "#a2ceea",
-      azul: "#1C6CAE",
-      ucla: "#3179B5",
-    },
+    // colors: {
+    //   powder: "#fffef8",
+    //   eggshell: "#efecda",
+    //   uranian: "#a2ceea",
+    //   azul: "#1C6CAE",
+    //   ucla: "#3179B5",
+    //   txt_primary: "#1f2937",
+    //   txt_second: "#6b7280",
+    // },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        powder: "#fffef8",
+        eggshell: "#efecda",
+        uranian: "#a2ceea",
+        azul: "#1C6CAE",
+        ucla: "#3179B5",
+        txt_primary: "#1f2937",
+        txt_second: "#6b7280",
       },
     },
   },
