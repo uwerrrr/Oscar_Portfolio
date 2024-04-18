b5f37d8d48f2e2d471fefb75aa4f4aefe003b037
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-04-18 23:29:59 +1000

    add sectionHeading and Wrapper

---
+++ portfolio-ver2/app/components/SectionWrapper.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+type SectionWrapperProps = {
+  children: React.ReactNode;
+  className?: string;
+  id?: string;
+  ref?: any;
+  otherProps?: React.HTMLAttributes<HTMLElement>;
+};
+
+export default function SectionWrapper({
+  children,
+  className,
+  id,
+  ref,
+  ...otherProps
+}: SectionWrapperProps) {
+  return (
+    <section
+      className={cn(
+        "scroll-mt-28 max-w-[800px] w-[77vw] min-h-[70vh] max-h-screen ",
+        className,
+        {}
+      )}
+      id={id}
+      ref={ref}
+      {...otherProps}
+    >
+      
+      {children}
+    </section>
+  );
+}
