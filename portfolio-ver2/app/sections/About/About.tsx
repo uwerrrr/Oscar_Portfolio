a17831d69469c23b3fed527d7dc67c7174f2618d
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-07-12 17:09:28 +1000

    Update About.tsx

--- portfolio-ver2/app/sections/About/About.tsx
+++ portfolio-ver2/app/sections/About/About.tsx
@@ -3,7 +3,34 @@ import React from "react";
 import SectionHeading from "@/app/components/SectionHeading";
 import { MotionDiv } from "@/app/components/MotionDiv";
 import SectionWrapper from "@/app/components/SectionWrapper";
-import SectionInView from "@/app/components/SectionInView";
+import { cn } from "@/lib/utils";
+
+const HighlightText = ({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) => {
+  return <span className={cn(className, `px-1 py-0 rounded`)}>{text}</span>;
+};
+
+const HobbyLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
+  <a
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="italic hover:rounded underline decoration-coral-500 hover:bg-uranian"
+  >
+    {children}
+  </a>
+);
 
 export default function About() {
   return (
@@ -16,7 +43,7 @@ export default function About() {
       >
         <SectionHeading className="">About me</SectionHeading>
 
-        <article className="grow text-justify text-lg flex flex-col justify-evenly gap-10">
+        <article className="grow text-justify text-base flex flex-col justify-evenly gap-10">
           <p className="">
             I&apos;m a tech enthusiast living in Sydney, proudly Saigonese at
             heart.
@@ -29,18 +56,19 @@ export default function About() {
           <p className="">
             My journey includes studies in Business Information Systems and Data
             Science, plus hands-on roles as an{" "}
-            <span className="font-semibold px-1 rounded bg-uranian">
-              ERP Consultant
-            </span>
-            ,{" "}
-            <span className="font-semibold px-1 rounded bg-uranian">
-              Data Scientist
-            </span>
-            , and{" "}
-            <span className="font-semibold px-1 rounded bg-uranian">
-              Software Engineer
-            </span>
-            . This blend has equipped me with a versatile toolkit: web
+            {["ERP Consultant", "Data Scientist", "Software Engineer"].map(
+              (role, i, arr) => (
+                <>
+                  <HighlightText
+                    key={role}
+                    text={role}
+                    className="bg-uranian"
+                  />
+                  {i < arr.length - 1 ? ", " : ". "}
+                </>
+              )
+            )}
+            This blend has equipped me with a versatile toolkit: web
             development, machine learning, data analysis, and business tech
             planning.
           </p>
@@ -54,85 +82,19 @@ export default function About() {
           </p>
           <p className="">
             Outside of work, you&apos;ll find me exploring city streets with my{" "}
-            <a
-              href="https://www.icloud.com/sharedalbum/#B1159UlCqCREDh"
-              target="_blank"
-              className="bg-ucla-200/80 rounded px-1 hover:underline hover:decoration-coral-500 hover:text-ucla-200/80 hover:bg-txt_primary-500"
-            >
+            <HobbyLink href="https://www.icloud.com/sharedalbum/#B1159UlCqCREDh">
               camera
-            </a>{" "}
+            </HobbyLink>{" "}
             or unwinding at home with my cats,{" "}
-            <a
-              href="https://www.icloud.com/sharedalbum/#B115yeZFhmnKdA"
-              target="_blank"
-              className="bg-ucla-200/80 rounded px-1 hover:underline hover:decoration-coral-500 hover:text-ucla-200/80 hover:bg-txt_primary-500"
-            >
+            <HobbyLink href="https://www.icloud.com/sharedalbum/#B115yeZFhmnKdA">
               Simba and Pumpkin
-            </a>
+            </HobbyLink>
             .
           </p>
           <p className="">
             Got an exciting project in mind? Let&apos;s connect and see what we
             can create together!
           </p>
-          {/* <p className="">
-            {`I'm proud to be a Sydneysider, but I'm a Saigonese at heart. As a
-            technology and data enthusiast, I have a genuine passion for
-            utilizing technologies to enhance business activities. With a
-            background in Machine Learning and ICT Business Analysis, and now
-            equipped with full-stack development skills, I am eager to build
-            technical solutions for any business challenges.`}
-          </p>
-
-          <p className="">
-            Also an amateur
-            <a
-              href="https://www.icloud.com/sharedalbum/#B1159UlCqCREDh"
-              className="link"
-              target="_blank"
-            >
-              <span className="link link__hidden-album">photographer</span>
-            </a>
-            .
-          </p>
-          <p className="">
-            And a cat lover too!
-            <a
-              href="https://www.icloud.com/sharedalbum/#B115yeZFhmnKdA"
-              className="link"
-              target="_blank"
-            >
-              <span className="link link__hidden-album">Simba & Pumpkin</span>
-            </a>
-            .
-          </p> */}
-          {/* <p className="mb-3">
-            [After graduating with a degree in{" "}
-            <span className="font-medium">Accounting</span>, I decided to pursue
-            my passion for programming. I enrolled in a coding bootcamp and
-            learned{" "}
-            <span className="font-medium">full-stack web development</span>.{" "}
-            <span className="italic">My favorite part of programming</span> is
-            the problem-solving aspect. I{" "}
-            <span className="underline">love</span> the feeling of finally
-            figuring out a solution to a problem. My core stack is{" "}
-            <span className="font-medium">
-              React, Next.js, Node.js, and MongoDB
-            </span>
-            . I am also familiar with TypeScript and Prisma. I am always looking
-            to learn new technologies. I am currently looking for a{" "}
-            <span className="font-medium">full-time position</span> as a
-            software developer.
-          </p>
-
-          <p>
-            <span className="italic">When I'm not coding</span>, I enjoy playing
-            video games, watching movies, and playing with my dog. I also enjoy{" "}
-            <span className="font-medium">learning new things</span>. I am
-            currently learning about{" "}
-            <span className="font-medium">history and philosophy</span>. I'm
-            also learning how to play the guitar.]
-          </p> */}
         </article>
       </MotionDiv>
     </SectionWrapper>
