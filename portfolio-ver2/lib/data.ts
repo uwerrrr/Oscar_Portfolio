4f4feb1d381e2fa4d8f51e96428a95106c0ea981
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2024-08-16 15:20:46 +1000

    Update data.ts

--- portfolio-ver2/lib/data.ts
+++ portfolio-ver2/lib/data.ts
@@ -83,44 +83,54 @@ export const experiencesData = [
   {
     title: "ERP Application Consultant",
     location: "Ho Chi Minh City, Vietnam",
-    description: `Worked in a team of 5 to deploy an ERP project for a major shoe manufacturing corporation in Vietnam, including the installation of ERP and database environments on the client’s server.
+    description: `Worked in a team of 5 to deploy an ERP project for a 2000-store shoe manufacturing corporation in Vietnam,
+including the installation of ERP and database environments on the client’s server.
 Engaged directly with client’s CFO to collect and analyse requirements, proposed ERP processes for the Finance
 module, and developed functional processes in accordance with Vietnamese Accounting Standards.
-Conducted user acceptance testing (UAT) phase, which included 5+ end-user training sessions and a detailed 10-
+Conducted user acceptance testing (UAT) phase, which included 7 end-user training sessions and a detailed 15-
 page user guideline.
-Proposed client-tailored solutions to overcome system limitation, utilising Excel Macro.
+Developed client-tailored solutions, improving operational efficiency by 25% using Excel Macro to overcome system
+limitations.
 Conducted 2 product pitching using Microsoft PowerBI.`,
     icon: faBriefcase,
     date: "Feb 2016 - Feb 2017",
   },
   {
-    title: "Data Scientist Intern",
+    title: "Data Scientist",
     location: "Sydney, NSW",
-    description: `Utilised Python and Mediapipe library to extend Truuth facekey biometrics from 128 to 468 3D face coordinates,
-enhancing the scope of potential facial verification tests.
+    description: `Did migration from Firebase Kit to Mediapipe library to extend Truuth facekey biometrics from 128 to 468 3D face
+coordinates, enhancing the scope of potential facial verification tests.
 Compiled 4 comprehensive documents outlining current implementations and potential future developments on
 internal Confluence platform.
 Contributed to a team of 5 in Scrum Agile environment to create a new app UX demo.
-Worked closely with a senior data scientist to boost the accuracy of Truuth MVP1 liveness fusion model, which was
-based on Mediapipe solution, from 65% to 75%.`,
+Worked closely with a senior data scientist to increase the accuracy of Truuth MVP1 liveness fusion model from
+65% to 75%, improving overall system reliability.
+TRG International, Ho Chi Minh City, Vietnam`,
     icon: faMicroscope,
     date: "Feb 2021 - Jun 2021",
   },
   {
-    title: "Full-Stack Software Engineer Trainee",
+    title: "Full-Stack Software Engineer",
     location: "Sydney, NSW",
-    description: `Completed an intensive 12-week training program on full-stack website development. Deployed 5+ projects using React with Javascript and Typescript, Java and MySQL. Coordinated with 2 senior software engineers on backend development for a client project, which involved a student
-metrics application using NestJS and Typescript. Enhanced CI/CD pipeline with Playwright end-to-end testing framework. Enriched data viewing requests by implementing 2 new HTTP requests using the MySQL virtual entities method.`,
+    description: `Completed an intensive 12-week training program on full-stack website development.
+Deployed 6 projects using React with Javascript and Typescript, Java and MySQL.
+Coordinated with 2 senior software engineers on backend development for a client project, which involved a student
+metrics application using NestJS and Typescript.
+Enhanced CI/CD pipeline by migrating to Playwright end-to-end testing framework.
+Enriched data viewing requests by implementing 2 new HTTP requests using the MySQL virtual entities method.`,
     icon: faLaptopCode,
     date: "May 2023 - Oct 2023",
   },
   {
-    title: "Full-Stack Software Engineer Intern",
+    title: "Full-Stack Software Engineer",
     location: "Sydney, NSW",
-    description: `Contributed to the development of a customer relationship management (CRM) Web Application project. Developed 3 pivotal features, including comprehensive unit testing functions, for a full-stack application built with
-NextJS, TypeScript, TailwindCSS, Drizzle ORM, Turso database and Stripe, ensuring robust functionality and
-adherence to best practices. Conducted comprehensive requirement gathering sessions with key stakeholders, including product managers and
-end-users, to capture and prioritize 10+ functional and non-functional requirements for a web project. Design system process flow with UML flowcharts to visualize complex processes, enabling cross-team alignment.`,
+    description: `Contributed to the development of a customer relationship management (CRM) Web Application project.
+Developed 3 pivotal features - document management, landing page and tax settings – along with unit testing
+functions for a full-stack application built with NextJS, TypeScript, ensuring robust functionality and adherence to
+best practices.
+Successfully captured and prioritized 10 functional and non-functional requirements, leading to a 20% increase in
+project delivery speed.
+Design system process flow with UML flowcharts to visualize complex processes, enabling cross-team alignment.`,
     icon: faLaptopCode,
     date: "Mar 2024 - Jun 2024",
   },
